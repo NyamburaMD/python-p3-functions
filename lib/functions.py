867985fc9c7d6e00e83810131e9b6f00e428769e
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-04-04 08:49:39 +0300

    complete assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
 
 def greet(name):
-    pass
+    print (f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)) :
+        return None
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+
